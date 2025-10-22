0b31861522da51f5e1be37ee326e6a2bf14a370d
oRodolfo <rodolforib.dev@hotmail.com>
2025-10-22 20:29:22 -0300

    Atualizando FrontEnd, BackEnd e o Banco de Dados e adicionando funcionalidades pendentes

--- teste.sql
+++ teste.sql
@@ -1,11 +1,11 @@
 -- Usuários
-SELECT id_usuario, nome, email FROM usuario ORDER BY id_usuario DESC LIMIT 10;
+SELECT id_usuario, nome, email FROM usuario ORDER BY id_usuario DESC LIMIT 1000;
 
 -- Doadores
-SELECT id_doador, id_usuario, data_cadastro FROM doador ORDER BY id_doador DESC LIMIT 10;
+SELECT id_doador, id_usuario, data_cadastro FROM doador ORDER BY id_doador DESC LIMIT 1000;
 
 -- Credenciais (ver login e último acesso)
-SELECT login, data_ultimo_login FROM credencial_usuario ORDER BY id_credencial DESC LIMIT 10;
+SELECT login, data_ultimo_login FROM credencial_usuario ORDER BY id_credencial DESC LIMIT 1000;
 
 -- Logs de auditoria
-SELECT id_log, data_hora, detalhes FROM log_auditoria ORDER BY id_log DESC LIMIT 20;
+SELECT id_log, data_hora, detalhes FROM log_auditoria ORDER BY id_log DESC LIMIT 2000;
